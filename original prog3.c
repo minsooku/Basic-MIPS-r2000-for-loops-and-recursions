fd3d8caaca77eed2d3bc918ff9c976bcba1e9fd7
minsooku <125935696+minsooku@users.noreply.github.com>
2023-06-03 16:49:31 +0900

    Create original prog3.c

---
+++ original prog3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int number= 0;
+int base= 0;
+int result= 0;
+
+static int num_digits(int n, int base);
+
+int main(void) {
+  scanf("%d", &number);
+  scanf("%d", &base);
+
+  result= num_digits(number, base);
+
+  printf("%d\n", result);
+
+  return 0;
+}
+
+static int num_digits(int n, int base) {
+  int ans;
+
+  ans= 0;
+
+  if (base <= 0)
+    ans= -1;
+  else {
+    if (n < 0)
+      n= -n;
+
+    if (base == 1)
+      ans= n;
+    else
+      if (n < base)
+        ans= 1;
+      else ans= 1 + num_digits(n / base, base);
+  }
+
+  return ans;
+}
